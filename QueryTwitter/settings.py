8ca60b58b7369bd474e6f2c5258ff6ed9261ec80
Manish Kumar Bobbili <manishkumar.bobbili3@gmail.com>
2020-02-03 23:24:33 -0800

    Procfile added

--- QueryTwitter/settings.py
+++ QueryTwitter/settings.py
@@ -113,9 +113,22 @@ USE_I18N = True
 USE_L10N = True
 
 USE_TZ = True
+AUTH_PASSWORD_VALIDATORS = [
+    {
+        "NAME": "django.contrib.auth.password_validation.UserAttributeSimilarityValidator"
+    },
+    {"NAME": "django.contrib.auth.password_validation.MinimumLengthValidator"},
+    {"NAME": "django.contrib.auth.password_validation.CommonPasswordValidator"},
+    {"NAME": "django.contrib.auth.password_validation.NumericPasswordValidator"},
+]
+
 
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
